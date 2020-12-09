53d305a543488824d3b3575ede028479d58e6b8d
Salomao dos Santos <salomaosan@gmail.com>
2020-12-09 20:44:22 -0300

    V1 portas tomcat para json

---
+++ zext_port_tomcat_row.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# vim: set ai ts=2 sw=2 expandtab :
+
+import  json, sys
+
+# Função que faz a atribuicao dos parametros passado caso haja falha os programa é encerrado.
+def atribuicao():
+    if sys.argv[1]:
+        global ports
+        ports = sys.argv[1].split(",")
+    else:
+        tomcat = []
+        data = {}
+        data["erro"] = "Sem dados"
+        tomcat.append(data)
+        print(json.dumps(tomcat, indent=4))
+        sys.exit()
+
+def main():
+    atribuicao()
+    data = {}
+    tomcats = []
+    for port in ports:
+        dado = {}
+        dado["{#PORT}"] = int(port)
+        dado["{#ERRO}"] = "Coleta OK"
+        tomcats.append(dado)  
+    data["data"] = tomcats
+    print(json.dumps(data, indent=4))
+
+if __name__ == '__main__': main()
